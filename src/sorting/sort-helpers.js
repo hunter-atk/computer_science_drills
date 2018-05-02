50a6b8049bf2770ebb68bdaae22c014fc907d4ed
Hunter Jefferson <hunterjefferson@Hunters-MacBook-Pro.local>
2018-05-02 14:02:28 -0500

    finished first two sorting algorithms

--- src/sorting/sort-helpers.js
+++ src/sorting/sort-helpers.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function swap(arr, idx1, idx2) {
-
+   var first = arr[idx1];
+   arr[idx1] = arr[idx2];
+   arr[idx2] = first;
+   return arr;
 }
 
 function merge(arr1, arr2) {
--- src/sorting/sortingAlgorithmsPart1.js
+++ src/sorting/sortingAlgorithmsPart1.js
@@ -3,11 +3,32 @@
 const swap = require('./sort-helpers').swap;
 
 function bubbleSort(arr) {
-
+  var swapped;
+  do {
+    swapped = false;
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] && arr[i + 1] && arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        swapped = true;
+      }
+    }
+  } while(swapped);
+  return arr;
 }
 
 function selectionSort(arr) {
-
+  for(var i = 0; i < arr.length; i++) {
+    var min = i;
+    for(var j = i + 1; j < arr.length; j++) {
+      if(arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if(i !== min) {
+      swap(arr, i, min);
+    }
+  }
+  return arr;
 }
 
 function insertionSort(arr) {
